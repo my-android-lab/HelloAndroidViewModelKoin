42ce6111ce707d761e6ed4a4c64b6e1058b2eeaf
sungcheol-kim <sungcheol@dable.io>
2019-09-11 12:38:16 +0900

    Adjust import statement for viewModel

--- app/src/main/java/com/example/helloandroidviewmodelkoin/module/AppModule.kt
+++ app/src/main/java/com/example/helloandroidviewmodelkoin/module/AppModule.kt
@@ -3,7 +3,7 @@ package com.example.helloandroidviewmodelkoin.module
 import com.example.helloandroidviewmodelkoin.MainViewModel
 import com.example.helloandroidviewmodelkoin.model.HelloRepository
 import com.example.helloandroidviewmodelkoin.model.HelloRepositoryImpl
-import org.koin.core.context.GlobalContext.get
+import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
