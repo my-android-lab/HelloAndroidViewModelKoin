451eaa9c97fee65835611bc93948dc6da56bbe19
sungcheol-kim <sungcheol@dable.io>
2019-09-11 12:41:01 +0900

    Lazy injecting android view model

--- app/src/main/java/com/example/helloandroidviewmodelkoin/MainActivity.kt
+++ app/src/main/java/com/example/helloandroidviewmodelkoin/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.helloandroidviewmodelkoin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+    //WOW! Lazy inject view model!
+    val mainViewModel: MainViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
