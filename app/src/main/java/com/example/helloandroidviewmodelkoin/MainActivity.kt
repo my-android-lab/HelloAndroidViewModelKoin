7820c745d89dc90f1763c8381de407fc6f76dfd1
sungcheol-kim <sungcheol@dable.io>
2019-09-11 12:41:51 +0900

    Use viewmodel

--- app/src/main/java/com/example/helloandroidviewmodelkoin/MainActivity.kt
+++ app/src/main/java/com/example/helloandroidviewmodelkoin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.helloandroidviewmodelkoin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView.text = mainViewModel.sayHello()
     }
 }
